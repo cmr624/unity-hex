80d0cb787b5e410699e64268c7982404763cb0f6
Carlos Michael Rodriguez <cmr624@nyu.edu>
2020-04-03 20:27:16 -0400

    project upload

---
+++ Assets/Scripts/HexMetrics.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class HexMetrics
+{
+   public const float outerRadius = 10f;
+   public const float innerRadius = outerRadius * 0.866025104f;
+
+   // corner pointing up, corner vectors
+   public static Vector3[] corners =
+   {
+      new Vector3(0f, 0f, outerRadius),
+      new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+      new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+      new Vector3(0f, 0f, -outerRadius),
+      new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+      new Vector3(-innerRadius, 0f, 0.5f * outerRadius),
+      new Vector3(0f, 0f, outerRadius),
+      
+   };
+
+}
