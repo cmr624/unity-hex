35f0fb7bac2715747f4c30b4869cb988abdbe0b5
Carlos Michael Rodriguez <cmr624@nyu.edu>
2020-04-05 12:02:58 -0400

    finished 3.1.3

--- Assets/Scripts/HexCell.cs
+++ Assets/Scripts/HexCell.cs
@@ -9,17 +9,26 @@ public class HexCell : MonoBehaviour
     public HexCoordinates coordinates;
     public Color color;
 
+    public RectTransform uiRect;
+    
+    
     //height of cell
-
     public int Elevation
     {
         get { return elevation; } 
         set
         {
           elevation = value;
+          
+          // hex cell position change
           Vector3 position = transform.localPosition;
           position.y = value * HexMetrics.elevationStep;
           transform.localPosition = position;
+          
+          // ui position change
+          Vector3 uiPosition = uiRect.localPosition;
+          uiPosition.z = elevation * -HexMetrics.elevationStep;
+          uiRect.localPosition = uiPosition;
         } 
     }
 
